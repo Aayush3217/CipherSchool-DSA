08cd7d2aa0061a605b62cd8c5ab828c3c88bf16d
Aayush pal <Aayushpal3217@gmail.com>
2026-02-11 10:46:28 +0530

    DSA PROBLEM

--- 2DArray/SetMatrixZeroes.cpp
+++ 2DArray/SetMatrixZeroes.cpp
@@ -1,6 +1,7 @@
 // Link : https://leetcode.com/problems/set-matrix-zeroes/
 
-class Solution {
+// brute force approach
+class Solution { 
 public:
     void solve(vector<vector<int>>& matrix, int i, int j){
         int rows = matrix.size();
@@ -28,6 +29,57 @@ public:
 
         for(auto& p : zeros){
             solve(matrix, p.first, p.second);
+        }
+    }
+};
+
+
+// Optimal Approach
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        bool zeroInTop = false;
+        bool zeroInLeft = false;
+
+        //check top row is there any zero
+        for(int i=0; i<m; i++){
+            if(matrix[0][i]==0)
+                zeroInTop = true;
+        }
+        // check left column is there any zeros
+        for(int i=0; i<n; i++){
+            if(matrix[i][0]==0)
+                zeroInLeft = true;
+        }
+        
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // make all zeros
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]= 0;
+            }
+        }
+
+        if(zeroInTop){
+            for(int i=0; i<m; i++){
+                matrix[0][i] = 0;
+            }
+        }
+        if(zeroInLeft){
+            for(int i=0; i<n; i++)
+                matrix[i][0] = 0;
         }
     }
 };
